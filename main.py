b39bc409b699490ebb324ba55de8f3a4b2a8c7cc
Florent Michel <florent.m42@gmail.com>
2025-10-05 18:09:21 +0100

    add a simple interaction with the model

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+model = os.environ.get("MODEL")
+
+client = genai.Client(api_key=api_key)
+
+# Test that we can communicate with the model
+response = client.models.generate_content(
+    model=model,
+    contents="Hi! What's the date today?")
+print(response.text)
