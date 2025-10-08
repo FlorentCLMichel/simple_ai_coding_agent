9fc8cf9144cdbefe094d8d0242d6ebcef9ba9b2c
Florent Michel <florent.m42@gmail.com>
2025-10-08 22:29:26 +0100

    add an example project

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY: clean
+
+clean:
+	rm -rf __pycache__
+	rm -rf test_calculator/__pycache__
+	rm -rf test_calculator/pkg/__pycache__
--- main.py
+++ main.py
@@ -9,9 +9,14 @@ model = os.environ.get("MODEL")
 
 client = genai.Client(api_key=api_key)
 
-if len(sys.argv) < 2 :
-    print("Missing argument: Prompt")
+if (len(sys.argv) < 2) or ("--help" in sys.argv[1:]) or ("-h" in sys.argv[1]) :
+    print("Use: python3 main.py <promt> <options>")
+    print("Options:")
+    print("  --help, -h    : Display this help message")
+    print("  --verbose, -v : Verbose mode")
     exit(1)
+
+verbose_mode = ("--verbose" in sys.argv[1:]) or ("-v" in sys.argv[1:])
 
 user_prompt = sys.argv[1]
 
@@ -23,5 +28,8 @@ response = client.models.generate_content(
     model=model,
     contents=messages)
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if verbose_mode : 
+    print("")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
