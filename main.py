74d200bfc27b65155852bde7236715eabbf73bb3
Florent Michel <florent.m42@gmail.com>
2025-10-25 10:55:33 +0100

    allow direct answers

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from functions.schemas import *
 
 system_prompt = '''
 You are a helpful AI coding agent.
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+When a user asks a question or makes a request, you may either provide a direct answer or make a function call plan. You can perform the following operations:
 - List files and directories.
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 '''
